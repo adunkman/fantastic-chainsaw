04aa9e696cc96832a8aa2914316e253f1d27ce65
Andrew Dunkman <adunkman@gmail.com>
2019-09-12 14:28:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello World from NodeJS on port ${port} from container ${os.hostname()}\n`);
+  res.end(`Hello World! from NodeJS on port ${port} from container ${os.hostname()}\n`);
 });
 
 server.listen(port, () => {
